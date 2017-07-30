a37a4ed06383c36d9da8a08e71e25467d16e44a1
0xEAB <desisma@heidel.beer>
2017-07-30 19:03:46 +0200

    Move PlatformUtil from <0xEAB/DCD_Parser>
    and rename it to RuntimePlatform

---
+++ source/RuntimePlatform.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CoE.em8.Core
+{
+    public static class RuntimePlatform
+    {
+        public static bool IsUnix
+        {
+            get;
+            private set;
+        }
+
+        public static PlatformID PlatformID
+        {
+            get;
+            private set;
+        }
+
+        public static string UserHomeDirectory
+        {
+            get { return Environment.GetFolderPath(Environment.SpecialFolder.Personal); }
+        }
+
+        static RuntimePlatform()
+        {
+            PlatformID = Environment.OSVersion.Platform;
+
+            /**
+             *   4 ... Unix
+             *   6 ... OS X
+             * 128 ... Unix (old versions of Mono)
+             */
+            int p = (int)PlatformID;
+            IsUnix = ((p == 4) || (p == 6) || (p == 128));
+        }
+    }
+}
